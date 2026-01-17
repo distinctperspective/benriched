89aded78460fd959ef1a315c23eb6a34e96893cb
safetychain <dthreatt@safetychain.com>
2026-01-17 02:36:36 -0600

    Revert "Add /discover endpoint for Core TAM company discovery"
    This reverts commit bd365049d5e11f4f3f253e828f5dc5cf2b96db7f.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import { cors } from 'hono/cors';
 import { logger } from 'hono/logger';
 import healthRoutes from './routes/health.js';
 import enrichRoutes from './routes/enrich.js';
-import discoverRoutes from './routes/discover.js';
 import { authMiddleware } from './middleware/auth.js';
 import { rateLimitMiddleware } from './middleware/rateLimit.js';
 
@@ -29,8 +28,5 @@ app.route('/health', healthRoutes);
 app.use('/enrich', rateLimitMiddleware);
 app.use('/enrich', authMiddleware);
 app.route('/enrich', enrichRoutes);
-
-app.use('/discover', authMiddleware);
-app.route('/discover', discoverRoutes);
 
 export default app;
